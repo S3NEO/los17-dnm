c1f92e03a01b994490dfea9ce47aa86dd68b28ca
RomanDesigner <romashkarus08@gmail.com>
2019-12-27 19:23:12 +0300

    Upload source for s3ve3gjv, s3ve3gds

---
+++ device/samsung/s3ve3gds/vendorsetup.sh
@@ -0,0 +1,20 @@
+echo "Checking for S3 Neo vendors..."
+( 
+FILE=vendor/samsung/s3ve3g-common/Android.mk
+if test -f "$FILE"; then
+    echo "s3ve3g-common: vendor exists [ 1 / 3 ]"; else
+    echo "s3ve3g-common: download vendor from github! Your ROM wont boot! Abort..."
+    exit 77
+fi
+FILE=vendor/samsung/s3ve3gds/Android.mk
+if test -f "$FILE"; then
+    echo "s3ve3gds: vendor exists [ 2 / 3 ]"; else
+    echo "s3ve3gds: download vendor from github! Your ROM wont boot! Abort..."
+    exit 77
+fi
+FILE=vendor/samsung/msm8226-common/Android.mk
+if test -f "$FILE"; then
+    echo "msm8226-common: vendor exists [ 3 / 3 ]"; else
+    echo "msm8226-common: download vendor from github! Your ROM wont boot! Abort..."
+    exit 77
+fi )
